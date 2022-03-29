9bf9e7a849cfa13893535f7c9b5154514a8fba99
Vann Harley <vannkharley@gmail.com>
2022-03-30 07:43:19 +1300

    Small Aesthetic updates Now the program returns a decimal value instead of a whole number

--- HI-JeanInternational/Program.cs
+++ HI-JeanInternational/Program.cs
@@ -6,15 +6,18 @@ namespace HI_JeanInternational
 {
     class Program
     {
-        static IDictionary<string, float> namesAndRatings = new Dictionary<string, float>();
+        static IDictionary<string, double> namesAndRatings = new Dictionary<string, double>();
         static List<string> CHEMICALLIST = new List<string>() { };
         static void Main(string[] args)
         {
             //Start Menu
-            Console.WriteLine("Welcome.\n" +
-                "Would you like to enter the data yourself or use the computer's randomly generated values?\n" +
-                "1. Enter the Data Myself.\n" +
-                "2. Use randomly generated values.");
+            Console.WriteLine("-------------------------------------------------------------------------\n" +
+                "Welcome.\n" +
+                "Use this App to calculate the efficiency rating of chemicals.\n" +
+                "-------------------------------------------------------------------------\n" +
+                "Please choose an option:\n" +
+                "1. Enter the number of germs myself.\n" +
+                "2. Use a randomly generated number of germs.");
             int userOrComputer = InputChecker(1,2);
 
             //Run the method once so that they get at least one chemical done
@@ -26,7 +29,7 @@ namespace HI_JeanInternational
             {
                 Console.WriteLine("Do you want to test another chemical?\n" +
                     "1: Yes\n" +
-                    "2: No\n");
+                    "2: No");
                 int stopOrGo = InputChecker(1,2);
 
                 if (stopOrGo == 1)
@@ -41,33 +44,33 @@ namespace HI_JeanInternational
             }
 
             //Rank all the chemicals and diplay the info to the user
-            Console.WriteLine("The final rating for each chemical ranked form lowest to highest:");
-            foreach (KeyValuePair<string, float> chemical in namesAndRatings.OrderBy(key => key.Value))
+            Console.WriteLine("\nThe final rating for each chemical ranked form lowest to highest:");
+            foreach (KeyValuePair<string, double> chemical in namesAndRatings.OrderBy(key => key.Value))
             {
                 Console.WriteLine($"Chemical Name: {chemical.Key}, Rating: {chemical.Value}");
             }
 
-            Console.WriteLine("Thank you for using my program. Have a nice day!");
+            Console.WriteLine("\nThank you for using my program. Have a nice day!");
             
 
         }
         static void RatingCalculator(int userOrComputer) 
         {
-            int numGerms = 0;
-            List<float> CHEMICALRATINGS = new List<float>() { };
+            double numGerms = 0;
+            List<double> CHEMICALRATINGS = new List<double>() { };
 
             //User inputs the Data
-            Console.WriteLine("What Chemical are you testing?");
+            Console.WriteLine("\nWhat Chemical are you testing?");
             string chemicalName = ChemicalChecker();
 
             //A comment so that the user knows what is happening
-            Console.WriteLine("The following process will repeat 5 times for a more accurate result.\n");
+            Console.WriteLine("\nThe following process will repeat 5 times for a more accurate result.\n");
 
             for (int i = 1; i < 2; i++)
             {
                 if (userOrComputer == 1)
                 {
-                   Console.WriteLine($"{i}: How many germs are you starting off with?");
+                   Console.WriteLine($"\n{i}: How many germs are you starting off with?");
                     numGerms = InputChecker(1,1000);
                 }
                 else
@@ -77,19 +80,19 @@ namespace HI_JeanInternational
                    Console.WriteLine($"{i}: You are starting off with {numGerms} germs.");
                 }
                 Console.WriteLine($"{i}: {chemicalName} is being added to {numGerms} germs. How long (in seconds) was this done for?");
-                int killTime = InputChecker(1,600);
+                float killTime = InputChecker(1,600);
 
                 Console.WriteLine($"{i}: How many Germs were left after the time was up?");
                 int remainingGerms = InputChecker(1,1000);
 
                 //The rating is calculated
-                float chemRating = (numGerms - remainingGerms) / killTime;
+                double chemRating = Math.Round((numGerms - remainingGerms) / killTime,3);
 
                 //The rating is added to the list containing all the ratings
                 CHEMICALRATINGS.Add(chemRating);
             }
             //Average rating is calculated
-            float finalChemAverage = Queryable.Average(CHEMICALRATINGS.AsQueryable());
+            double finalChemAverage = Queryable.Average(CHEMICALRATINGS.AsQueryable());
             Console.WriteLine($"The final rating for {chemicalName} is {finalChemAverage}\n");
 
             //Chemical and it's rating is added to list containing all chemicals and list containing all ratings
