ce245f2fbd6db30de1aa4b674c08175256b8bf95
Vann Harley <vannkharley@gmail.com>
2022-03-31 12:42:33 +1300

    Aesthetics updated

--- HI-JeanInternational/Program.cs
+++ HI-JeanInternational/Program.cs
@@ -6,15 +6,17 @@ namespace HI_JeanInternational
 {
     class Program
     {
+        //Global Variables
         static IDictionary<string, double> namesAndRatings = new Dictionary<string, double>();
         static List<string> CHEMICALLIST = new List<string>() { };
         static void Main(string[] args)
         {
             //Start Menu
-            Console.WriteLine("-------------------------------------------------------------------------\n" +
+            Console.WriteLine("" +
+                "><><><><><>><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><\n" +
                 "Welcome.\n" +
                 "Use this App to calculate the efficiency rating of chemicals.\n" +
-                "-------------------------------------------------------------------------\n" +
+                "><><><><><>><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><\n" +
                 "Please choose an option:\n" +
                 "1. Enter the number of germs myself.\n" +
                 "2. Use a randomly generated number of germs.");
@@ -44,46 +46,50 @@ namespace HI_JeanInternational
             }
 
             //Rank all the chemicals and diplay the info to the user
-            Console.WriteLine("\nThe final rating for each chemical ranked form lowest to highest:");
+            Console.WriteLine("\n><><><><><>><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><\n" +
+                "The final rating for each chemical ranked form lowest to highest:");
             foreach (KeyValuePair<string, double> chemical in namesAndRatings.OrderBy(key => key.Value))
             {
                 Console.WriteLine($"Chemical Name: {chemical.Key}, Rating: {chemical.Value}");
             }
 
-            Console.WriteLine("\nThank you for using my program. Have a nice day!");
+            Console.WriteLine("\nThank you for using my program. Have a nice day!\n" +
+                "><><><><><>><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><");
             
 
         }
         static void RatingCalculator(int userOrComputer) 
         {
-            double numGerms = 0;
+            int numGerms = 0;
             List<double> CHEMICALRATINGS = new List<double>() { };
 
             //User inputs the Data
-            Console.WriteLine("\nWhat Chemical are you testing?");
+            
             string chemicalName = ChemicalChecker();
 
             //A comment so that the user knows what is happening
-            Console.WriteLine("\nThe following process will repeat 5 times for a more accurate result.\n");
+            Console.WriteLine("The following process will repeat 5 times for a more accurate result.\n" +
+                "-------------------------------------------------------------------------");
 
-            for (int i = 1; i < 2; i++)
+            for (int i = 1; i < 6; i++)
             {
                 if (userOrComputer == 1)
                 {
-                   Console.WriteLine($"\n{i}: How many germs are you starting off with?");
+                   Console.WriteLine($"How many germs are you starting off with?");
                     numGerms = InputChecker(1,1000);
                 }
                 else
                 {
                    Random rnd = new Random();
                    numGerms = rnd.Next(1, 1001);
-                   Console.WriteLine($"{i}: You are starting off with {numGerms} germs.");
+                   Console.WriteLine($"You are starting off with {numGerms} germs.");
                 }
-                Console.WriteLine($"{i}: {chemicalName} is being added to {numGerms} germs. How long (in seconds) was this done for?");
+                Console.WriteLine($"{chemicalName} is being added to {numGerms} germs. How long (in seconds) was this done for?");
                 float killTime = InputChecker(1,600);
 
-                Console.WriteLine($"{i}: How many Germs were left after the time was up?");
-                int remainingGerms = InputChecker(1,1000);
+                Console.WriteLine($"How many Germs were left after the time was up?");
+                int remainingGerms = InputChecker(0,numGerms);
+                Console.WriteLine("-------------------------------------------------------------------------");
 
                 //The rating is calculated
                 double chemRating = Math.Round((numGerms - remainingGerms) / killTime,3);
@@ -92,7 +98,7 @@ namespace HI_JeanInternational
                 CHEMICALRATINGS.Add(chemRating);
             }
             //Average rating is calculated
-            double finalChemAverage = Queryable.Average(CHEMICALRATINGS.AsQueryable());
+            double finalChemAverage = Math.Round(Queryable.Average(CHEMICALRATINGS.AsQueryable()),3);
             Console.WriteLine($"The final rating for {chemicalName} is {finalChemAverage}\n");
 
             //Chemical and it's rating is added to list containing all chemicals and list containing all ratings
@@ -100,6 +106,7 @@ namespace HI_JeanInternational
             CHEMICALLIST.Add(chemicalName);
         }
 
+        // A method that makes sure the user cannot break the program with erroneous or boundry case data
         static int InputChecker(int valueMin, int valueMax)
         {
             while(true) 
@@ -113,25 +120,33 @@ namespace HI_JeanInternational
                     }
                     else 
                     {
-                        Console.WriteLine($"ERROR! Must be an integer between {valueMin} and {valueMax}.\n");
+                        Console.WriteLine($"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n" +
+                            $"ERROR! Must be an integer between {valueMin} and {valueMax}.\n" +
+                            $"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                     }
                 }
                 catch
                 {
-                    Console.WriteLine($"ERROR! Must be an integer between {valueMin} and {valueMax}.\n");
+                    Console.WriteLine($"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n" +
+                        $"ERROR! Must be an integer between {valueMin} and {valueMax}.\n" +
+                        $"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 }
             }
         }
 
+        // A method that makes sure the user cannot enter the same chemical twice
         static string ChemicalChecker() 
         {
             while (true)
             {
+                Console.WriteLine("\nWhat Chemical are you testing?");
                 string userInput = Console.ReadLine();
 
                 if (CHEMICALLIST.Contains(userInput))
                 {
-                    Console.WriteLine("ERROR! You cannot enter the same Chemical twice.");
+                    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n" +
+                        "ERROR! You cannot enter the same Chemical twice.\n" +
+                        "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
                 }
                 else 
                 {
